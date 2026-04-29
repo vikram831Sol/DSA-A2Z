55bef0ed427628ea1bd18b8ff543c0a09bb7bdd2
Vikram Singh <831vikram@gmail.com>
2026-04-29 05:42:46 +0000

    armstrong problem solution

---
+++ basic_maths/isarmstrong.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool armstrong(int n){
+    int temp=n;
+    int k;
+    int size=0;
+    int sum=0;
+    do
+    {
+        size++;
+        temp=temp/10;
+    } while (temp);
+    temp=n;
+    do
+    {
+        k=temp%10;
+        int p=1;
+        for (int i = 1; i <=size; i++)
+        {
+            p=p*k;
+        }
+        
+        sum=sum+p;
+        temp=temp/10;
+}while (temp);
+
+return sum==n;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int c;
+    c=armstrong(n);
+    cout << c << "\n";
+    return 0;
+}
