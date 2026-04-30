01b8645af07335e2f10293c2d97aff5086536777
Vikram Singh <831vikram@gmail.com>
2026-04-30 03:04:18 +0000

    modified isPrime problem

--- basic_maths/isPrime.cpp
+++ basic_maths/isPrime.cpp
@@ -12,6 +12,23 @@ bool isPrime(int n){
     return true;  
 }
 
+//sieve method for getting array of prime numbers till number n
+// vector<bool> sieve(int n) {
+//     vector<bool> isPrime(n + 1, true);
+
+//     isPrime[0] = isPrime[1] = false;
+
+//     for (int i = 2; i * i <= n; i++) {
+//         if (isPrime[i]) {
+//             for (int j = i * i; j <= n; j += i) {
+//                 isPrime[j] = false;
+//             }
+//         }
+//     }
+
+//     return isPrime;
+// }
+
 int main()
 {
     int n;
