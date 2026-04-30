68fd2a0d3c7dfd9d587295c53d2b39b3115685b2
Vikram Singh <831vikram@gmail.com>
2026-04-30 03:01:52 +0000

    added isPrime Problem

---
+++ basic_maths/isPrime.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+    if (n<=1){return false;}
+    for (int i = 2; i*i <=n; i++)
+    {
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;  
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int c;
+    c=isPrime(n);
+    cout << c << "\n";
+    return 0;
+}
