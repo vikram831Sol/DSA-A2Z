9536c768a3500fff6fb7fcd3ce10a076131de976
Vikram Singh <831vikram@gmail.com>
2026-06-21 09:38:04 +0000

    added problem description in the max product problem

--- arrays/maxProduct.cpp
+++ arrays/maxProduct.cpp
@@ -1,3 +1,22 @@
+/*
+
+Given an integer array nums. Find the subarray with the largest product, and 
+return the product of the elements present in that subarray.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+Example 1
+Input: nums = [4, 5, 3, 7, 1, 2]
+Output: 840
+Explanation:
+The largest product is given by the whole array itself
+
+Example 2
+Input: nums = [-5, 0, -2]
+Output: 0
+Explanation:
+The largest product is achieved with the following subarrays [0], [-5, 0], [0, -2], [-5, 0, -2].
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
