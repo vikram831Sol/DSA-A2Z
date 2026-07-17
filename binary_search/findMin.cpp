a71a0decaabeb0e19956e62788f133d78695b81b
Vikram Singh <831vikram@gmail.com>
2026-07-17 14:20:26 +0000

    added find min in the sorted array rotated

---
+++ binary_search/findMin.cpp
@@ -0,0 +1,57 @@
+/*
+Given an integer array nums of size N, sorted in ascending order with distinct values,
+ and then rotated an unknown number of times (between 1 and N), find the minimum element
+in the array.
+Example 1
+Input : nums = [4, 5, 6, 7, 0, 1, 2, 3]
+Output: 0
+Explanation: Here, the element 0 is the minimum element in the array.
+
+Example 2
+Input : nums = [3, 4, 5, 1, 2]
+Output: 1
+Explanation:Here, the element 1 is the minimum element in the array.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int> &arr)  {
+      int low=0;
+      int high=arr.size()-1;
+      int mini=INT_MAX;
+      while (low<=high)
+      {
+        int mid=low+((high-low)/2);
+        if (arr[low]<=arr[mid])//if left side is sorted take the min
+        //arr[low]temporarily for now and later then sort out the search space to unsorted part
+        //that is right side.
+        {
+            (mini>=arr[low])?mini=arr[low]:mini=mini;
+            low=mid+1;
+        }
+        //if right side is sorted take the min arr[mid]for now and do the searching in
+        //unsorted part that is left side.
+        else{
+            (mini>=arr[mid])?mini=arr[mid]:mini=mini;
+            high=mid-1;
+        }
+      }
+      
+      return mini;
+    }
+
+int main()
+{
+    int size;
+    cin>>size;
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+    int ans=findMin(arr);
+    cout << ans << endl;
+    
+    return 0;
+}
